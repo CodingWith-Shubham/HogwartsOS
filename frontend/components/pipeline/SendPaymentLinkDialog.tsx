61db093a25319602d1d9838c9fb4f269b5b3c354
CodingWithShubham <mamgainshubham18@gmail.com>
2026-08-21 00:47:58 +0530

    improve send payment link ui

--- frontend/components/pipeline/SendPaymentLinkDialog.tsx
+++ frontend/components/pipeline/SendPaymentLinkDialog.tsx
@@ -183,7 +183,7 @@ export function SendPaymentLinkDialog({
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="sm:max-w-md">
+      <DialogContent className="sm:max-w-md max-h-[90vh] overflow-y-auto">
         <DialogHeader>
           <DialogTitle>Send Payment Link</DialogTitle>
           <DialogDescription>
